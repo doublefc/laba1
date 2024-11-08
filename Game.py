54453d24ab809d4e1ec8e02577285ab379f17709
Stepan Prohorov <108407596+doublefc@users.noreply.github.com>
2024-11-08 19:13:38 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,9 +1,8 @@
 class Game:
 
-    def __init__(self,  available_games, n_trials=3, n_rounds=3):
+    def __init__(self,  available_games, n_rounds=3):
         self.n_rounds = n_rounds
         self.available_games = available_games
-        self.n_trials = n_trials
     
     def __base_io(self):
         print('Привет! Добро пожаловать в Brain Games!')
@@ -13,15 +12,15 @@ class Game:
         if self.game_str.isnumeric():
             self.game_str = list(self.available_games.keys())[int(self.game_str)-1]
 		
-    def launch(self, new_user=True):        
-        if new_user:
-            self.__base_io()
+    def launch(self):  
+	    
+        self.__base_io()
+	    
         for _ in range(self.n_rounds):
             question, right_answer = self.available_games[self.game_str]()
-            for _ in range(self.n_trials):
-                user_answer = float(input(question + '\n Твой ответ:'))                
-                if user_answer == right_answer:
-                    print('Молодец, верно')
+            user_answer = float(input(question + '\n Твой ответ:'))                
+            if user_answer == right_answer:
+                print('Молодец, верно')
             else:
                 print(f'Количество попыток кончилось, правильный ответ был {right_answer}')
             
